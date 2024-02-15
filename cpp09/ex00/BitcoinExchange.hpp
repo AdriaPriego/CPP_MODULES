21b47c7b3894b3754cfa722a08dbc70d7eece9e5
AdriaPriego <73168012+AdriaPriego@users.noreply.github.com>
2024-02-15 14:48:21 +0000

    ex00 done

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 17:05:41 by codespace         #+#    #+#             */
-/*   Updated: 2024/02/14 18:13:12 by codespace        ###   ########.fr       */
+/*   Updated: 2024/02/15 14:35:02 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,12 @@
 typedef struct s_exchange
 {
     struct tm tm;
-    double qtt;
+    double eRate;
 }           t_exchange;
 
-bool test_int(const std::string literal);
+int dayDifference(const struct tm &date1, const struct tm &date2)
+{
+    int date1Acum = date1.tm_year * 365 + date1.tm_mon * 30 + date1.tm_mday;
+    int date2Acum = date2.tm_year * 365 + date2.tm_mon * 30 + date2.tm_mday;
+    return (date1Acum - date2Acum);
+}
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 17:05:44 by codespace         #+#    #+#             */
-/*   Updated: 2024/02/14 18:23:56 by codespace        ###   ########.fr       */
+/*   Updated: 2024/02/15 14:34:59 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,49 +26,93 @@ std::vector<std::string> split(const std::string& s, char delimiter) {
     return tokens;
 }
 
-int parseFile(std::vector<std::string> fileVector)
+std::vector<t_exchange> parseBD(std::vector<std::string> fileVector)
 {
-    std::vector<std::string> test;
-    std::vector<std::string>::iterator init = fileVector.begin();
+    std::vector<std::string>::iterator itFile = fileVector.begin();
     std::vector<t_exchange> exc;
-    if (*init == "date | value")
-        init++;
-    while (init != fileVector.end())
+    if (*itFile == "date,exchange_rate")
+        itFile++;
+    while (itFile != fileVector.end())
     {
-        std::vector<std::string> test = split(*init, '|');
-        std::vector<std::string>::iterator init2 = test.begin();
+        std::vector<std::string> test = split(*itFile, ',');
+        std::vector<std::string>::iterator itLine = test.begin();
         bool first = true;
-        std::cout << *init << std::endl;
+        bool valid = true;        
         t_exchange testing;
-        while (init2 != test.end())
+        while (itLine != test.end())
         {
             if (first)
             {
-                if (strptime((*init2).c_str(), "%Y-%m-%d ", &testing.tm))
-                {
-                    std::cout << "Año: " << testing.tm.tm_year + 1900 << '\n';
-                    std::cout << "Mes: " << testing.tm.tm_mon + 1 << '\n';
-                    std::cout << "Día: " << testing.tm.tm_mday << '\n';
-                }
-                else
-                    std::cout << "Bad date." << std::endl;
+                if (!strptime((*itLine).c_str(), "%Y-%m-%d", &testing.tm))
+                    valid = false;
             }
             else
             {
-                    testing.qtt = std::atof(&(*init2)[1]);
-                    if (testing.qtt > INT_MAX || testing.qtt <= 0)
-                        std::cout << "Bad Number" << std::endl;
-                    else
-                        std::cout << testing.qtt << std::endl;
+                testing.eRate = std::atof((*itLine).c_str());
+                if (testing.eRate > INT_MAX || testing.eRate < 0)
+                    valid = false;
             }
             first = false;
-            init2++;
+            itLine++;
         }
-        exc.push_back(testing);
-        init++;
+        if (valid == true)
+            exc.push_back(testing);
+        itFile++;
     }
-    return (0);
+    return (exc);
 }
+//2011-01-03 | 3
+t_exchange parseLine(std::string line)
+{
+    t_exchange exc;
+    std::vector<std::string> lineSplit = split(line, '|');
+
+    if (lineSplit.size() != 2)
+        throw std::invalid_argument("Error: bad input => " + line);
+    if (!strptime(lineSplit[0].c_str(), "%Y-%m-%d ", &exc.tm))
+        throw std::invalid_argument(line + "  Error: not a valid date.");
+    exc.eRate = std::atof(&(lineSplit[1][1]));
+    if (exc.eRate > 1000)
+        throw std::out_of_range(line + "  Error: too large a number.");
+    if (exc.eRate < 0)
+        throw std::out_of_range(line + "  Error: not a positive number.");
+    return (exc);
+}
+
+void doExchange(std::vector<t_exchange> dataBD, std::vector<std::string> fileVector)
+{
+    std::vector<std::string>::iterator it = fileVector.begin();
+    std::vector<t_exchange>::iterator minDate;
+    int diff = INT_MAX;
+    int auxdiff;
+    if (*it == "date | value")
+        it++;
+    while (it != fileVector.end())
+    {
+        try
+        {
+            t_exchange exc = parseLine(*it);
+            std::vector<t_exchange>::iterator itDB = dataBD.begin();
+            while (itDB != dataBD.end())
+            {
+                auxdiff = dayDifference(exc.tm, (*itDB).tm);
+                if (auxdiff < diff && auxdiff >= 0)
+                {
+                    diff = auxdiff;
+                    minDate = itDB;
+                }
+                itDB++;
+            }
+            std::cout << exc.tm.tm_year + 1900 << "-" << exc.tm.tm_mon + 1 << "-" << exc.tm.tm_mday << " => " << exc.eRate << " = " << exc.eRate * (*minDate).eRate << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+        } 
+        it++;
+    }
+}
+
 
 int main(int argc, char **argv)
 {
@@ -78,11 +122,11 @@ int main(int argc, char **argv)
         return (1);
     }
     std::ifstream file;
-
-    file.open(argv[1], std::ios::in);
+    (void)argv;
+    file.open("data.csv", std::ios::in);
     if (!file)
     {
-        std::cout << "Invalid File" << std::endl;
+        std::cout << "BD not found" << std::endl;
         return (1);
     }
     std::vector<std::string> fileVector;
@@ -92,6 +136,19 @@ int main(int argc, char **argv)
         fileVector.push_back(line);
     }
     file.close();
-    parseFile(fileVector); 
+    std::vector<t_exchange> dataBD = parseBD(fileVector);
+    file.open(argv[1], std::ios::in);
+    if (!file)
+    {
+        std::cout << "File not found" << std::endl;
+        return (1);
+    }
+    fileVector.clear();
+    while (getline(file, line))
+    {
+        fileVector.push_back(line);
+    }
+    file.close();
+    doExchange(dataBD, fileVector);
     return (0);       
 }
